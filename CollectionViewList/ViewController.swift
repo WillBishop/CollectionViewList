9621cf488dcf5ffda80418ce3a4cabd940d06c48
Will Bishop <17292672+WillBishop@users.noreply.github.com>
2022-03-11 11:08:26 +1030

    Added Image Type

--- CollectionViewList/ViewController.swift
+++ CollectionViewList/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 enum Section: Hashable {
 	case info
+	case image
 }
 
 enum Row: Hashable {
 	case name
 	case description
+	case image(image: UIImage)
 }
 
 class DiffableDataSource: UICollectionViewDiffableDataSource<Section, Row> {
@@ -39,10 +41,15 @@ class ViewController: UIViewController {
 		let textRegistration = UICollectionView.CellRegistration<TextInputCell, Row> { (cell, indexPath, item) in
 			cell.item = item
 		}
+		let imageRegistration = UICollectionView.CellRegistration<ImageCell, Row> { (cell, indexPath, item) in
+			cell.item = item
+		}
 		self.dataSource = DiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
 			switch itemIdentifier {
 			case .name, .description:
 				return collectionView.dequeueConfiguredReusableCell(using: textRegistration, for: indexPath, item: itemIdentifier)
+			case .image:
+				return collectionView.dequeueConfiguredReusableCell(using: imageRegistration, for: indexPath, item: itemIdentifier)
 			default:
 				fatalError()
 			}
@@ -51,6 +58,8 @@ class ViewController: UIViewController {
 		var snapshot = self.dataSource.snapshot()
 		snapshot.appendSections([.info])
 		snapshot.appendItems([.name, .description], toSection: .info)
+		snapshot.appendSections([.image])
+		snapshot.appendItems([.image(image: UIImage(named: "displayImage")!)], toSection: .image)
 		self.dataSource.apply(snapshot, animatingDifferences: false)
 	}
 	
@@ -147,5 +156,88 @@ class TextInputView: UIView, UIContentView {
 		// Replace current configuration with new configuration
 		currentConfiguration = configuration
 		self.textField.placeholder = configuration.placeholder
+	}
+}
+
+class ImageCell: UICollectionViewListCell {
+	
+	var item: Row?
+	
+	override func updateConfiguration(using state: UICellConfigurationState) {
+		var newConfiguration = ImageCellConfiguration().updated(for: state)
+		guard let item = item else {
+			return
+		}
+		switch item {
+		case .image(let image):
+			newConfiguration.image = image
+		default:
+			newConfiguration.image = nil
+		}
+		contentConfiguration = newConfiguration
+	}
+	
+}
+
+struct ImageCellConfiguration: UIContentConfiguration, Hashable {
+	
+	var image: UIImage?
+
+	func makeContentView() -> UIView & UIContentView {
+		return ImageCellView(configuration: self)
+	}
+	
+	func updated(for state: UIConfigurationState) -> ImageCellConfiguration {
+		return self
+	}
+}
+
+class ImageCellView: UIView, UIContentView {
+	
+	private var currentConfiguration: ImageCellConfiguration!
+	var configuration: UIContentConfiguration {
+		get { currentConfiguration }
+		set {
+			guard let configuration = newValue as? ImageCellConfiguration else {
+				return
+			}
+			self.apply(configuration: configuration)
+			
+		}
+	}
+	
+	var display = UIImageView()
+	
+	init(configuration: ImageCellConfiguration) {
+		super.init(frame: .zero)
+		self.setupViews()
+		self.apply(configuration: configuration)
+	}
+	
+	func setupViews() {
+		self.addSubview(display)
+		display.translatesAutoresizingMaskIntoConstraints = false
+		display.contentMode = .scaleAspectFill
+		NSLayoutConstraint.activate([
+			display.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+			display.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+			display.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+			display.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
+			display.heightAnchor.constraint(equalToConstant: 200)
+		])
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	private func apply(configuration: ImageCellConfiguration) {
+		guard currentConfiguration != configuration else {
+			return
+		}
+		
+		// Replace current configuration with new configuration
+		currentConfiguration = configuration
+		self.display.image = configuration.image
 	}
 }
